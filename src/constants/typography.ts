94f08ec79e8dcc1b77fa0c5c27aca28c6bef4361
Kae <kaehehehe@gmail.com>
2025-03-07 19:47:47 +0900

    initial setup

---
+++ src/constants/typography.ts
@@ -0,0 +1,103 @@
+import { css } from "@emotion/react";
+
+export const Typographies = {
+  topHeadline: {
+    B: css`
+      font-size: 26px;
+      line-height: 32px;
+      font-weight: 700;
+    `,
+    SB: css`
+      font-size: 26px;
+      line-height: 32px;
+      font-weight: 600;
+    `,
+  },
+  headline: {
+    B: css`
+      font-size: 24px;
+      line-height: 28px;
+      font-weight: 700;
+    `,
+    SB: css`
+      font-size: 24px;
+      line-height: 28px;
+      font-weight: 600;
+    `,
+  },
+  subHead: {
+    B: css`
+      font-size: 20px;
+      line-height: 26px;
+      font-weight: 700;
+    `,
+    SB: css`
+      font-size: 20px;
+      line-height: 26px;
+      font-weight: 600;
+    `,
+  },
+  title: {
+    B: css`
+      font-size: 18px;
+      line-height: 24px;
+      font-weight: 700;
+    `,
+    SB: css`
+      font-size: 18px;
+      line-height: 24px;
+      font-weight: 600;
+    `,
+  },
+  semiTitle: {
+    B: css`
+      font-size: 16px;
+      line-height: 22px;
+      font-weight: 700;
+    `,
+    SB: css`
+      font-size: 16px;
+      line-height: 22px;
+      font-weight: 600;
+    `,
+  },
+  body: {
+    B: css`
+      font-size: 14px;
+      line-height: 18px;
+      font-weight: 700;
+    `,
+    SB: css`
+      font-size: 14px;
+      line-height: 18px;
+      font-weight: 600;
+    `,
+    R: css`
+      font-size: 14px;
+      line-height: 16px;
+      font-weight: 400;
+    `,
+  },
+  caption: {
+    B: css`
+      font-size: 12px;
+      line-height: 16px;
+      font-weight: 700;
+    `,
+    SB: css`
+      font-size: 12px;
+      line-height: 16px;
+      font-weight: 600;
+    `,
+    R: css`
+      font-size: 12px;
+      line-height: 16px;
+      font-weight: 400;
+    `,
+  },
+  subCaption: css`
+    font-size: 10px;
+    line-height: 14px;
+    font-weight: 400;
+  `,
+} as const;
