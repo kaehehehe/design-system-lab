c2c09136e278bd961feb687668cfcbcf6e2d67d3
Kae <kaehehehe@gmail.com>
2025-05-02 18:08:32 +0900

    chore: enables Vanilla Extract CSS support in Storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,19 +1,24 @@
-import type { StorybookConfig } from '@storybook/react-vite';
+import type { StorybookConfig } from "@storybook/react-vite";
+import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
     "@chromatic-com/storybook",
-    "@storybook/experimental-addon-test"
+    "@storybook/experimental-addon-test",
   ],
-  "framework": {
-    "name": "@storybook/react-vite",
-    "options": {}
-  }
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  async viteFinal(config) {
+    config.plugins = config.plugins || [];
+
+    config.plugins.push(vanillaExtractPlugin());
+
+    return config;
+  },
 };
 export default config;
